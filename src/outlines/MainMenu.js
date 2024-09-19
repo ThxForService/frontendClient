4a57bd8d7976df7950ddd737a18b855aab0e1204
Yunda0204 <a01034353045@gmail.com>
2024-09-19 17:15:52 +0900

    .

--- src/outlines/MainMenu.js
+++ src/outlines/MainMenu.js
@@ -1,81 +1,41 @@
-import Link from 'next/link';
-import { useTranslation } from 'next-i18next';
-import styled from 'styled-components';
+'use client';
+import React from 'react';
+import { getCommonStates } from '@/commons/contexts/CommonContext';
+import styled, { css } from 'styled-components';
+import { useTranslation } from 'react-i18next';
 
-const MenuBox = styled.nav`
-  background: white;
-  width: 100%;
-  max-width: 1440px;
-  margin: 0 auto;
-  padding: 0 20px;
-`;
-
-const MenuItem = styled.div`
-  position: relative;
-  display: inline-block;
-  margin: 0 10px;
-
-  a {
-    color: dark;
-    text-decoration: none;
-    &:hover {
-      color: midGreen;
+const Menus = styled.nav`
+  background: ${({ theme }) => theme.colors.black};
+  .inner {
+    display: flex;
+    height: 45px;
+    a {
+      line-height: 45px;
+      fontsize: ${({ theme }) => theme.fontSizes.mediumLarge}px;
+      color: ${({ theme }) => theme.colors.white};
+      padding: 0 35px;
     }
-  }
-
-  &:hover .sub-menu {
-    display: block;
-  }
-`;
-
-const SubMenu = styled.div`
-  position: absolute;
-  background: white;
-  display: none;
-  width: 200px;
-  z-index: 1000;
-  a {
-    color: dark;
-    &:hover {
-      background: lightGreen;
+    a.on,
+    a:hover {
+      background: ${({ theme }) => theme.colors.gray};
     }
-  }
-`;
-const Logo = styled.div`
-  display: flex;
-  align-items: center;
-  margin: 0 10px;
-
-  img {
-    width: 180px;
-    height: auto;
   }
 `;
 
 const MainMenu = () => {
+  const { showMainMenu } = getCommonStates();
   const { t } = useTranslation();
 
   return (
-    <MenuBox>
-      <MenuItem>
-        <a href="/counseling/reserve">{t('예약')}</a>
-        <SubMenu className="sub-menu">
-          <a href="/counseling/reserve">{t('예약 신청')}</a>
-          <a href="/counseling/list">{t('나의 예약 현황')}</a>
-        </SubMenu>
-      </MenuItem>
-      <MenuItem>
-        <a href="/menu2">{t('메뉴2')}</a>
-        <SubMenu className="sub-menu">
-          <a href="/submenu3">{t('서브 메뉴3')}</a>
-          <a href="/submenu4">{t('서브 메뉴4')}</a>
-        </SubMenu>
-      </MenuItem>
-      <Logo>
-        <Link href="/">로고</Link>
-      </Logo>
-    </MenuBox>
+    showMainMenu && (
+      <Menus>
+        <div className="layout-width inner">
+          <a href="#">{t('메뉴1')}</a>
+          <a href="#">{t('메뉴2')}</a>
+        </div>
+      </Menus>
+    )
   );
 };
 
-export default MainMenu;
+export default React.memo(MainMenu);
