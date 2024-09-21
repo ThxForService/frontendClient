4d1489f075129c2e5b3e20094b278235777a2f7e
irine3355 <irine3355@gmail.com>
2024-09-21 13:13:09 +0900

    메인 로그인 페이지 2차

--- src/app/page.js
+++ src/app/page.js
@@ -4,6 +4,9 @@ import { getCommonActions } from '@/commons/contexts/CommonContext';
 import LoginContainer from '@/member/containers/LoginContainer';
 import GuestOnlyContainer from '@/member/containers/GuestOnlyContainer';
 import { OuterBox } from '@/commons/layouts/StyledWrapper';
+import Link from 'next/link';
+import Image from 'next/image';
+import logo from '@/images/logo.png';
 
 import './main.scss';
 
@@ -25,6 +28,11 @@ export default function Home({ searchParams }) {
       setBgClass((prev) => {
         if (prev === 'bg1') return 'bg2';
         if (prev === 'bg2') return 'bg3';
+        if (prev === 'bg3') return 'bg4';
+        if (prev === 'bg4') return 'bg5';
+        if (prev === 'b5') return 'bg6';
+        if (prev === 'b6') return 'bg7';
+        if (prev === 'b7') return 'bg8';
         return 'bg1';
       });
     }, 4000); // 4초마다 배경 변경
@@ -37,9 +45,26 @@ export default function Home({ searchParams }) {
       <div className={`background ${bgClass}`} />{' '}
       {/* 배경 이미지 애니메이션이 적용될 div */}
       <GuestOnlyContainer className="no-zoom">
-        <OuterBox>
-          <LoginContainer searchParams={searchParams} />
-        </OuterBox>
+        <div className="login-logobox">
+          {' '}
+          {/* 새로 추가한 클래스 */}
+          <Link href="/" passHref>
+            <div className="login-header">
+              <Image
+                className="hover-effect"
+                src={logo}
+                alt="로고 이미지"
+                width={100}
+                height={100}
+              />
+              <span className="hover-effect">당신의, 당신에 의한,</span>
+              <span className="hover-effect">당신을 위한 상담합니다!</span>
+            </div>
+          </Link>
+          <OuterBox>
+            <LoginContainer searchParams={searchParams} />
+          </OuterBox>
+        </div>
       </GuestOnlyContainer>
     </div>
   );
