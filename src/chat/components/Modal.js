57ff365da6b3154ba2121bf93da53a41e997d3a5
jeongsuri <jeongsuri96@gmail.com>
2024-09-26 12:19:33 +0900

    feat: 오류 수정

--- src/chat/components/Modal.js
+++ src/chat/components/Modal.js
@@ -17,7 +17,7 @@ const ModalContainer = styled.div`
     height: 690px;
     background-color: white;
     padding: 20px;
-    border-radius: 8px;
+    border-radius: 15px;
     box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
     z-index: 1050;
     position: fixed;
