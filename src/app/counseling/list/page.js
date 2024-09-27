e2cf36863ff3223baeb1416793f21bce51444337
Yunda0204 <a01034353045@gmail.com>
2024-09-27 13:59:05 +0900

    .

--- src/app/counseling/list/page.js
+++ src/app/counseling/list/page.js
@@ -1,13 +1,22 @@
-'use client'
-import React from 'react';
+'use client';
 import MemberOnlyContainer from '@/member/containers/MemberOnlyContainer';
 import CounselingListContainer from '@/counseling/containers/CounselingListContainer';
+import Header from '@/commons/layouts/Header';
+import {
+  ContentBox,
+  ContentBox2,
+  ContentBox3,
+} from '@/commons/layouts/StyledWrapper';
 
-const CounselingView = () => {
+const CounselingView = ({ searchParams }) => {
   return (
     <MemberOnlyContainer>
-      <CounselingListContainer />
+      <Header />
+      <ContentBox3>
+        <CounselingListContainer searchParams={searchParams} />
+      </ContentBox3>
     </MemberOnlyContainer>
   );
 };
-export default React.memo(CounselingView);
+
+export default CounselingView;
