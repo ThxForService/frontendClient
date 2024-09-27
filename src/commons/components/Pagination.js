f0eb4c8ec47eb796fe6cac9c490789482a3f9cd7
Yunda0204 <a01034353045@gmail.com>
2024-09-27 10:49:35 +0900

    .

--- src/commons/components/Pagination.js
+++ src/commons/components/Pagination.js
@@ -36,3 +36,45 @@ const Wrapper = styled.div`
     color: #fff;
   }
 `;
+const Pagination = ({ pagination, onClick }) => {
+  const { page, pages, prevRangePage, nextRangePage, totalPages } = pagination;
+
+  return (
+    pages.length > 0 && (
+      <Wrapper>
+        {prevRangePage > 0 && (
+          <>
+            <MdFirstPage onClick={() => onClick(1)} className="page" />
+            <MdNavigateBefore
+              onClick={() => onClick(Number(prevRangePage))}
+              className="page"
+            />
+          </>
+        )}
+        {pages.map((p) => (
+          <div
+            key={'page' + p[0]}
+            onClick={() => onClick(Number(p[0]))}
+            className={'page' + classNames({ ' on': Number(p[0]) === page })}
+          >
+            {p[0]}
+          </div>
+        ))}
+        {nextRangePage > 0 && (
+          <>
+            <MdNavigateNext
+              onClick={() => onClick(Number(nextRangePage))}
+              className="page"
+            />
+            <MdLastPage
+              onClick={() => onClick(Number(totalPages))}
+              className="page"
+            />
+          </>
+        )}
+      </Wrapper>
+    )
+  );
+};
+
+export default React.memo(Pagination);
