030a9464a0f0ac035fcc150b98cb5eb2d2ed253e
jeongsuri <jeongsuri96@gmail.com>
2024-09-27 11:27:03 +0900

    feat : css 적용중

--- src/main/components/Mainlogo.js
+++ src/main/components/Mainlogo.js
@@ -1,49 +1,49 @@
 import React from 'react';
 import Image from 'next/image';
 import styled from 'styled-components';
-import smilelogo from '/public/images/smilelogo.png'; // 이미지 경로
+import smilelogo from '/public/images/logo.png'; // 이미지 경로
 
 // LogoContainer: 로고를 포함하는 컨테이너
 const LogoContainer = styled.div`
-  display: flex;
-  align-items: center;
-  margin-left: 650px;
-  margin-top: -4px;
-  position: absolute;
+    display: flex;
+    align-items: center;
+    justify-content: center; // 중앙 정렬
+    height: 100%; // MenuContainer의 높이와 같게 설정
 `;
 
 // 로고 이미지 스타일링
 const LogoImage = styled(Image)`
-  width: 85px;
-  height: auto;
-  cursor: pointer;
-  &:focus,
-  &:hover {
-    outline: none;
-  }
+    width: 85px;
+    height: auto;
+    cursor: pointer;
+    margin-top: 35px;
+    &:focus,
+    &:hover {
+        outline: none;
+    }
 `;
 
 // 외부에서 사용할 텍스트 스타일 (MainMenu에서 사용)
 export const StyledLogoText = styled.a`
-  color: #000000;
-  font-size: 1.5em;
-  font-weight: bold;
-  margin-left: 10px;
-  text-decoration: none;
-  line-height: 100px;
-  margin-top: 0px;
+    color: #000000;
+    font-size: 1.5em;
+    font-weight: bold;
+    margin-left: 10px;
+    text-decoration: none;
+    line-height: 100px;
+    margin-top: 0px;
 
-  &:focus,
-  &:hover {
-    outline: none;
-  }
+    &:focus,
+    &:hover {
+        outline: none;
+    }
 `;
 
 const Mainlogo = () => {
   return (
     <LogoContainer>
       <a
-        href="http://localhost:7000/"
+        href="/"
         target="_self"
         style={{ outline: 'none' }}
       >
